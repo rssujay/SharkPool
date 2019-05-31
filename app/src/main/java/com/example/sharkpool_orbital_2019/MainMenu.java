700bd6c900e085062946f64d1e4daf606a7a8a35
Rohan Arya Varma <rohanaryavarma@gmail.com>
2019-05-31 18:02:56 +0800

    Place temporary default Borrow Request Array and include Sharkpool logo

--- app/src/main/java/com/example/sharkpool_orbital_2019/MainMenu.java
+++ app/src/main/java/com/example/sharkpool_orbital_2019/MainMenu.java
@@ -27,8 +27,11 @@ public class MainMenu extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                /*
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                */
+
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
