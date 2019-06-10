72927e47d7393edf97f2cdb6415a22e6a25e4ed9
rssujay <40201586+rssujay@users.noreply.github.com>
2019-06-10 18:39:56 +0800

    Unadd realm files

--- app/src/main/java/com/example/sharkpool_orbital_2019/AppUser.java
+++ app/src/main/java/com/example/sharkpool_orbital_2019/AppUser.java
@@ -1,17 +1,7 @@
 package com.example.sharkpool_orbital_2019;
 
-import com.google.firebase.auth.FirebaseAuth;
-
-import io.realm.RealmObject;
-
-public class AppUser extends RealmObject {
+public class AppUser{
+    private String uid; //Unique ID to be used as primary key
     private String displayName;
     private String emailAddress;
-    private String uid; //Unique ID to be used as primary key
-
-    public AppUser(){
-        displayName = FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
-        emailAddress = FirebaseAuth.getInstance().getCurrentUser().getEmail();
-        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-    }
 }
