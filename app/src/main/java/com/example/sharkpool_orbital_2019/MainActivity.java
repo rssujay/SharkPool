bc8159fb9c010ccf30f83101178a70f83d5146e3
rssujay <40201586+rssujay@users.noreply.github.com>
2019-07-13 12:29:34 +0800

    Rework app startup

--- app/src/main/java/com/example/sharkpool_orbital_2019/MainActivity.java
+++ app/src/main/java/com/example/sharkpool_orbital_2019/MainActivity.java
@@ -4,76 +4,97 @@ import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.sendbird.android.SendBird;
 import com.sendbird.android.SendBirdException;
 import com.sendbird.android.User;
 
-
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    private int clickCount = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
-        final Button signIn = findViewById(R.id.signinbtn);
+        mAuth = FirebaseAuth.getInstance();
+    }
 
-        // Proceed to main menu immediately if email verified and logged in
-         if (FirebaseAuth.getInstance().getUid() != null && FirebaseAuth.getInstance().getCurrentUser().isEmailVerified()){
-             SendBird.connect(FirebaseAuth.getInstance().getUid(), new SendBird.ConnectHandler() {
-                 @Override
-                 public void onConnected(User user, SendBirdException e) {
-                     if (e != null){ //error
-                         Toast.makeText(getApplicationContext(), "Failed to connect to messaging service. Check your connection and try again.", Toast.LENGTH_LONG).show();
-                     }
-                 }
-             });
-             // adding displayName as Nickname in SendBird database
-             SendBird.updateCurrentUserInfo(FirebaseAuth.getInstance().getCurrentUser().getDisplayName().toString(), null, new SendBird.UserInfoUpdateHandler() {
-                 @Override
-                 public void onUpdated(SendBirdException e) {
-                     if (e != null) { //error
-                         Toast.makeText(getApplicationContext(), "Failed to update chat nickname to "+FirebaseAuth.getInstance().getCurrentUser().getDisplayName(), Toast.LENGTH_LONG).show();
-                     }
-                 }
-             });
-             Intent intent = new Intent(this, MainMenu.class);
-             startActivity(intent);
-         }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUser = mAuth.getCurrentUser();
 
-         signIn.setOnClickListener(
-             new View.OnClickListener() {
-                 int count = 0;
-                 @Override
-                 public void onClick(View v) {
-                     if (FirebaseAuth.getInstance().getCurrentUser() == null || FirebaseAuth.getInstance().getCurrentUser().getUid().isEmpty()){
-                         Intent intent = new Intent(v.getContext(), LoginActivity.class);
-                            startActivity(intent);
-                     }
+        if (currentUser != null && currentUser.isEmailVerified()){
+            proceedToMainMenu();
+        }
+    }
 
-                     if (FirebaseAuth.getInstance().getCurrentUser() != null) {
-                         FirebaseAuth.getInstance().getCurrentUser().reload();
-                     }
-                     if (!FirebaseAuth.getInstance().getCurrentUser().isEmailVerified()){
-                         if (count == 0) {
-                             FirebaseAuth.getInstance().getCurrentUser().sendEmailVerification();
-                             Toast.makeText(v.getContext(), "Verification email sent - please verify and retry", Toast.LENGTH_LONG).show();
-                             count++;
-                         }
-                         else{
-                             Toast.makeText(v.getContext(), "Ensure that you have verified and retry in a minute", Toast.LENGTH_LONG).show();
-                         }
-                     }
+    private void proceedToMainMenu(){
+        //Connect user to SendBird
+        SendBird.connect(currentUser.getUid(), new SendBird.ConnectHandler() {
+            @Override
+            public void onConnected(User user, SendBirdException e) {
+                // Handle exceptions
+                if (e != null){
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Failed to connect to messaging service. Check your connection and try again.", Toast.LENGTH_LONG).show();
+                }
 
-                     else{
-                         Intent intent = new Intent(v.getContext(), MainMenu.class);
-                         startActivity(intent);
-                     }
-                 }
-             });
+                else{
+                    // add displayName as Nickname in SendBird database
+                    SendBird.updateCurrentUserInfo(currentUser.getDisplayName(), null, new SendBird.UserInfoUpdateHandler() {
+                        @Override
+                        public void onUpdated(SendBirdException e) {
+                            if (e != null) {
+                                Toast.makeText(getApplicationContext(),
+                                        "Failed to update chat nickname to "
+                                                + currentUser.getDisplayName(), Toast.LENGTH_LONG).show();
+                            }
+
+                            else{
+                                Intent intent = new Intent(getApplicationContext(), MainMenu.class);
+                                startActivity(intent);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    public void redirectUser(View v){
+        clickCount++;
+
+        // If not registered/signed in
+        if (currentUser == null){
+            Intent intent = new Intent(v.getContext(), LoginActivity.class);
+            startActivity(intent);
+        }
+
+        // Else if signed in but email is not verified
+        else if (currentUser != null && !currentUser.isEmailVerified()){
+            if (clickCount == 1){
+                currentUser.sendEmailVerification();
+                Toast.makeText(v.getContext(), "Verification email sent - please verify and retry", Toast.LENGTH_LONG).show();
+            }
+
+            else{
+                Toast.makeText(v.getContext(), "Ensure that you have verified and retry in a minute", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        //Else for some reason the user has not been redirected to mainMenu
+        else{
+            proceedToMainMenu();
+        }
     }
 }
