08d197413c2e6a9a64c2dcb08c1cff55e013a66d
rssujay <40201586+rssujay@users.noreply.github.com>
2019-07-13 16:01:41 +0800

    Wave theme creation part 2

--- app/src/main/java/com/example/sharkpool_orbital_2019/LoginActivity.java
+++ app/src/main/java/com/example/sharkpool_orbital_2019/LoginActivity.java
@@ -32,6 +32,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
 
         // Firebase login process
         String uid = FirebaseAuth.getInstance().getUid();
