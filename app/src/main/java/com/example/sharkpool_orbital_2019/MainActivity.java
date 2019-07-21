cc41f190a1d709d946b0d7dabd8f28dea428fec7
Rohan Arya Varma <rohanaryavarma@gmail.com>
2019-07-21 13:19:30 +0800

    Add background animation for login page

--- app/src/main/java/com/example/sharkpool_orbital_2019/MainActivity.java
+++ app/src/main/java/com/example/sharkpool_orbital_2019/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.sharkpool_orbital_2019;
 
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -38,6 +41,25 @@ public class MainActivity extends AppCompatActivity {
             notifRedirect = getIntent().getExtras().getString("requestID");
             getIntent().removeExtra("requestID");
         }
+
+        final ImageView backgroundOne = (ImageView) findViewById(R.id.background_one);
+        final ImageView backgroundTwo = (ImageView) findViewById(R.id.background_two);
+
+        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(10000L);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float width = backgroundOne.getWidth();
+                final float translationX = width * progress;
+                backgroundOne.setTranslationX(translationX);
+                backgroundTwo.setTranslationX(-width + translationX);
+            }
+        });
+        animator.start();
     }
 
     @Override
